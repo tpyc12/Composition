63ebde96673de0dd1c6f24db235865243e8124f0
tpyc12 <tpyc12@yandex.ru>
2022-08-18 22:29:59 +0300

    feat: resources for application

---
+++ app/src/main/java/com/myhome/android/composition/presentation/ChooseLevelFragment.kt
@@ -0,0 +1,18 @@
+package com.myhome.android.composition.presentation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.myhome.android.composition.R
+
+class ChooseLevelFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_choose_level, container, false)
+    }
+}
