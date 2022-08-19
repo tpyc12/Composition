865239a733b3ae04326c85363ed9c5f3f6d5d3e0
tpyc12 <tpyc12@yandex.ru>
2022-08-19 20:02:48 +0300

    fix: change serializable on parcelable

--- app/src/main/java/com/myhome/android/composition/domain/entity/Level.kt
+++ app/src/main/java/com/myhome/android/composition/domain/entity/Level.kt
@@ -1,5 +1,9 @@
 package com.myhome.android.composition.domain.entity
 
-enum class Level {
-   TEST, EASY, NORMAL, HARD
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+enum class Level : Parcelable {
+    TEST, EASY, NORMAL, HARD
 }
